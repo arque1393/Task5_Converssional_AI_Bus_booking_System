589f1b8b370a576fcec423b9adcbde53a86e960c
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2024-03-29 14:30:22 +0530

    Adding Basic Constants requirements and gitignores

---
+++ WebApp/constants.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path 
+
+BASE_DIR = Path(__file__).resolve().parent
+CACHE_DATASET = BASE_DIR/'cache_dir'
+DATABASE_DIR = BASE_DIR/'ChromaDB'
+## Google API key 
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+# GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+# # # Model Selection     
+# LLM_MODEL_NAME = r'models/chat-bison-001'
+# LLM_MODEL_NAME = r'models/text-bison-001'
+LLM_MODEL_NAME = r'models/gemini-pro'
+# LLM_MODEL_NAME = r'models//gemini-pro-version'
+# LLM_MODEL_NAME = r'models/aqa'
